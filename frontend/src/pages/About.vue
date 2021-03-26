76918b406a063a9f8db3aaab64cb737f924422ab
sonworks <csw0413@outlook.com>
2021-03-26 20:16:45 +0900

    API connection test

--- frontend/src/pages/About.vue
+++ frontend/src/pages/About.vue
@@ -1,3 +1,32 @@
 <template>
-    <div class="About">About</div>
+    <div class="About">
+        <div>About</div>
+        <p v-text="greeting"></p>
+        <div><input @click="getGreeting" type="button" value="Say!"></div>
+    </div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      greeting: ''
+    }
+  },
+  methods: {
+    getGreeting () {
+      this.greeting = 'Waiting...'
+      this.$axios.get('http://localhost:8080/greeting')
+        .then((response) => {
+          console.log(response)
+          this.greeting = response.data
+        })
+        .catch((reason) => {
+          console.log(reason)
+          this.greeting = 'Failed to get data'
+        })
+    }
+  }
+}
+</script>
