9a3fc4bf44791b68cebc1d6e03b404df0f1c4242
sonworks <csw0413@outlook.com>
2021-03-27 18:52:26 +0900

    vuetify table ui sample

--- frontend-vuetify/src/pages/Sample.vue
+++ frontend-vuetify/src/pages/Sample.vue
@@ -50,9 +50,33 @@
       v-model="select"
       item-text="label"
       item-value="value"
-      :items="items"
+      :items="selectItems"
       label="select"
     />
+
+    <p>&nbsp;</p>
+    <h2># Alert</h2>
+    <v-alert type="success" value="true">Success</v-alert>
+    <v-alert type="info" value="true">Info</v-alert>
+    <v-alert type="warning" value="true">Warning</v-alert>
+    <v-alert type="error" value="true">Error</v-alert>
+
+    <p>&nbsp;</p>
+    <h2># Table</h2>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      class="elevation-5"
+    >
+    <template slot:item.calories="{ item }">
+      <v-chip
+        :color="getColor(item.calories)"
+        dark
+      >
+        {{ item.calories }}
+      </v-chip>
+    </template>
+    </v-data-table>
   </v-container>
 </template>
 
@@ -60,12 +84,114 @@
 export default {
   data () {
     return {
-      items: [
+      selectItems: [
         { label: 'select1', value: 'select1' },
         { label: 'select2', value: 'select2' },
         { label: 'select3', value: 'select3' }
-      ]
+      ],
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' },
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%',
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%',
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%',
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%',
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%',
+        },
+      ],
     }
-  }
+  },
+  methods: {
+    getColor (calories) {
+      if (calories > 400) return 'red'
+      else if (calories > 200) return 'orange'
+      else return 'green'
+    },
+  },
 }
 </script>
