d46c195a5e686fb14d81b28e0381eb2842919c26
sonworks <csw0413@outlook.com>
2021-03-27 19:14:35 +0900

    frontend-vuetify API connection test

--- frontend-vuetify/src/pages/About.vue
+++ frontend-vuetify/src/pages/About.vue
@@ -1,5 +1,32 @@
 <template>
   <v-container>
-    About
+    <p v-text="greeting"></p>
+    <v-btn color="primary" @click="getGreeting">Say!</v-btn>
   </v-container>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      greeting: ''
+    }
+  },
+  methods: {
+    getGreeting () {
+      this.greeting = 'Waiting...'
+      this.$axios.get('http://localhost:8080/greeting')
+        .then((response) => {
+          console.log(response)
+          var date = new Date()
+          this.greeting = response.data + ' / ' + date.toISOString()
+        })
+        .catch((reason) => {
+          console.log(reason)
+          this.greeting = 'Failed to get data'
+        })
+    }
+  }
+}
+</script>
